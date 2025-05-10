2860b3b74d30bb07699ccf006f3693f62c2d3766
omer_sm <smorodinskyomer@gmail.com>
2025-05-10 15:13:24 +0300

    chore(package.json): remove react gh pages package

--- src/Components/ReportModal/ReportModal.tsx
+++ src/Components/ReportModal/ReportModal.tsx
@@ -3,6 +3,8 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import Snackbar from '@mui/material/Snackbar';
 import TextField from '@mui/material/TextField';
 import { useTheme } from '@mui/material/styles';
@@ -13,7 +15,6 @@ import { formatReport } from '../../Utils/formatReport';
 import { useSnapshot } from 'valtio';
 import { reportState } from '../../State/ReportState/reportState';
 import './copyButtonAnimations.css';
-import { Stack, Typography } from '@mui/material'
 
 interface ReportModalProps {
   dialogOpen: boolean;
@@ -41,60 +42,60 @@ export default function ReportModal({ dialogOpen, setDialogOpen }: ReportModalPr
     <>
       <Dialog fullWidth={true} open={dialogOpen} onClose={() => setDialogOpen(false)}>
         <DialogTitle>
-          <Stack direction='row' justifyContent='space-between' alignItems='center'>
-            <Typography variant='h4'>הדוח מוכן !</Typography>
+          <Stack direction="row" justifyContent="space-between" alignItems="center">
+            <Typography variant="h4">הדוח מוכן !</Typography>
             <Button
-            color="success"
-            variant="contained"
-            onClick={copyReport}
-            sx={{
-              position: 'relative',
-              zIndex: 0,
-              '&:after': {
-                content: '""',
-                position: 'absolute',
-                left: 0,
-                top: 0,
-                zIndex: -10,
-                height: '100%',
-                width: '100%',
-                borderRadius: '16px',
-                backgroundColor: theme.palette.success.main,
-                transformOrigin: 'center',
-                transition: 'transform 0.5s, opacity 0.5s, background-color 0.3s',
-                transform:
-                  copyButtonState === 1 || copyButtonState === 2
-                    ? 'scaleX(1.4) scaleY(1.4)'
-                    : '',
-                opacity: copyButtonState > 0 ? 0 : 0.5,
-              },
-              '&:hover:after': {
-                backgroundColor: theme.palette.success.dark,
-              },
-              '&:focus:after': {
-                backgroundColor: theme.palette.success.main,
-              },
-            }}
-          >
-            {copyButtonState !== 2 ? (
-              <ContentCopyRoundedIcon
-                sx={{
-                  animation:
-                    copyButtonState === 1
-                      ? 'shrink 1s ease'
-                      : copyButtonState === 3
-                      ? 'grow 800ms ease'
-                      : undefined,
-                }}
-              />
-            ) : (
-              <CheckRoundedIcon
-                sx={{
-                  animation: copyButtonState === 2 ? 'grow 1s ease' : undefined,
-                }}
-              />
-            )}
-          </Button>
+              color="success"
+              variant="contained"
+              onClick={copyReport}
+              sx={{
+                position: 'relative',
+                zIndex: 0,
+                '&:after': {
+                  content: '""',
+                  position: 'absolute',
+                  left: 0,
+                  top: 0,
+                  zIndex: -10,
+                  height: '100%',
+                  width: '100%',
+                  borderRadius: '16px',
+                  backgroundColor: theme.palette.success.main,
+                  transformOrigin: 'center',
+                  transition: 'transform 0.5s, opacity 0.5s, background-color 0.3s',
+                  transform:
+                    copyButtonState === 1 || copyButtonState === 2
+                      ? 'scaleX(1.4) scaleY(1.4)'
+                      : '',
+                  opacity: copyButtonState > 0 ? 0 : 0.5,
+                },
+                '&:hover:after': {
+                  backgroundColor: theme.palette.success.dark,
+                },
+                '&:focus:after': {
+                  backgroundColor: theme.palette.success.main,
+                },
+              }}
+            >
+              {copyButtonState !== 2 ? (
+                <ContentCopyRoundedIcon
+                  sx={{
+                    animation:
+                      copyButtonState === 1
+                        ? 'shrink 1s ease'
+                        : copyButtonState === 3
+                        ? 'grow 800ms ease'
+                        : undefined,
+                  }}
+                />
+              ) : (
+                <CheckRoundedIcon
+                  sx={{
+                    animation: copyButtonState === 2 ? 'grow 1s ease' : undefined,
+                  }}
+                />
+              )}
+            </Button>
           </Stack>
         </DialogTitle>
         <DialogContent>
