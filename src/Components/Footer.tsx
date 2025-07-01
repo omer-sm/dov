d9c669b22e91e43a438ee1e549b3e04c65873061
omer-sm <sm7551444@gmail.com>
2025-07-01 15:36:12 +0300

    hotfix(Footer.tsx): fix dark mode

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,8 +1,9 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 
 export default function Footer() {
+  const theme = useTheme();
   return (
-    <Box height="4.5rem" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor="ButtonShadow">
+    <Box height="4.5rem" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor={theme.palette.mode === 'dark' ? 'ThreeDLightShadow' : 'buttonShadow'}>
       <Typography width="100%" textAlign="center" color="textSecondary" fontSize={17}>מערכת דוחות בטיחות / דו"ב</Typography>
       <Typography width="100%" textAlign="center" color="textSecondary" fontSize={15}>פותח על ידי עומר סמורודינסקי, וייפר</Typography>
     </Box>
