7bf8c17fc521b74d3cc410a716762bf654708211
omer-sm <sm7551444@gmail.com>
2025-07-01 12:14:12 +0300

    style(Footer.tsx): improve footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <Box height="4.5rem" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor="ButtonShadow">
       <Typography width="100%" textAlign="center" color="textSecondary" fontSize={17}>מערכת דוחות בטיחות / דו"ב</Typography>
-      <Typography width="100%" textAlign="center" color="textSecondary" fontSize={15}>פותח על ידי עומר סמורודינסקי</Typography>
+      <Typography width="100%" textAlign="center" color="textSecondary" fontSize={15}>פותח על ידי עומר סמורודינסקי, וייפר</Typography>
     </Box>
   )
 }
